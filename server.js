9041877ac29a0c153d5705795c5fc8c080a40ad0
Sachin Adat <sachinadat@gmail.com>
2026-06-27 01:49:30 +0530

    Add api for continent data

--- server.js
+++ server.js
@@ -10,10 +10,19 @@ const server = http.createServer(async (req, res) => {
         res.setHeader('Content-Type', 'application/json')
         res.statusCode = 200
         res.end(JSON.stringify(destinations))
+    } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
+        const continent = req.url.split('/').pop()
+        const filteredData = destinations.filter(d => d.continent.toUpperCase() === continent.toUpperCase())
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode = 200
+        res.end(JSON.stringify(filteredData))
     } else {
         res.setHeader('Content-Type', 'application/json')
         res.statusCode = 404
-        res.end(JSON.stringify({error: "not found", message: "The requested route does not exist"}))
+        res.end(JSON.stringify({
+            error: "not found",
+            message: "The requested route does not exist"
+        }))
     }
 
 })
