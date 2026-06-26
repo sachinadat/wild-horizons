58eb112d5005b4b19373136cd7d8e3c66cd4a57e
Sachin Adat <sachinadat@gmail.com>
2026-06-27 01:23:59 +0530

    Add response content type header & status codes

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const server = http.createServer(async (req, res) => {
     const destinations = await getDataFromDB()
 
     if (req.url === '/api' && req.method === 'GET') {
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode = 200
         res.end(JSON.stringify(destinations))
     } else {
-        res.end()
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode = 404
+        res.end(JSON.stringify({error: "not found", message: "The requested route does not exist"}))
     }
 
 })
